895deeafcbce794f4d48168a8b9c34cee2f5a833
romitglobal <romitglobal@gmail.com>
2025-11-08 00:22:12 +0530

    change connection

--- components/connect.php
+++ components/connect.php
@@ -2,9 +2,9 @@
 
   /* Conexão com a base de dados */
 
-$db_name = 'mysql:host=localhost;dbname=e-commerce-digital-products-base-dados';
-$user_name = 'root';
-$user_password = '';
+$db_name = 'mysql:host=localhost:3306;dbname=local_db';
+$user_name = 'pub_user';
+$user_password = 'Pubuser@789';
 
 
 $conn = new PDO($db_name, $user_name, $user_password);
